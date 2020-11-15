b6b42729051b0fc93123151ea852fd5016f7a007
D-Tomics <akm001gaming@gmail.com>
2020-11-15 20:00:29 +0530

    feat: ClassFileReader now also capable of reading a single file specified by the package name

--- src/main/java/com/dtomics/reflections/classReaders/ClassFileReader.java
+++ src/main/java/com/dtomics/reflections/classReaders/ClassFileReader.java
@@ -1,7 +1,6 @@
 package com.dtomics.reflections.classReaders;
 
 import com.dtomics.reflections.utils.ClassUtils;
-import com.dtomics.reflections.utils.ReflectionUtils;
 
 import java.io.File;
 import java.net.URL;
@@ -13,29 +12,41 @@ import java.util.List;
 import static com.dtomics.reflections.ReflectionConstants.CLASS_FILE_SUFFIX;
 
 public class ClassFileReader implements ClassReader {
+
     @Override
     public List<Class<?>> read(URL url, String packageName, ClassLoader classLoader) {
-        File dir = new File(URLDecoder.decode(url.getPath(),StandardCharsets.UTF_8));
-        return scanDir(dir, packageName, new ArrayList<>(), classLoader);
+        File dir = new File(URLDecoder.decode(url.getPath(), StandardCharsets.UTF_8));
+        List<Class<?>> dest = new ArrayList<>();
+        if (dir.exists()) {
+            if (dir.isDirectory()) {
+                return scanDir(dir, packageName, dest, classLoader);
+            } else {
+                if (dir.getName().endsWith(CLASS_FILE_SUFFIX)) {
+                    String fileName = dir.getName();
+                    dest.add(getClassFromFile(packageName.isEmpty() ? "%s%s" : "%s.%s", packageName.substring(0, packageName.length() - fileName.length() - 1), dir, classLoader));
+                }
+            }
+        }
+        return dest;
     }
 
     private List<Class<?>> scanDir(File dir, String packageName, List<Class<?>> dest, ClassLoader classLoader) {
-        if(dir.exists() && dir.isDirectory()) {
+        if (dir.exists() && dir.isDirectory()) {
             File[] files = dir.listFiles();
-            if(files == null) return null;
+            if (files == null) return null;
 
             String format = packageName.isEmpty() ? "%s%s" : "%s.%s";
-            for(File file : files) {
+            for (File file : files) {
                 String fileName = file.getName();
-                if(file.isDirectory())
+                if (file.isDirectory()) {
                     scanDir(file, String.format(format, packageName, fileName), dest, classLoader);
-                if(fileName.endsWith(CLASS_FILE_SUFFIX)) {
+                }
+                if (fileName.endsWith(CLASS_FILE_SUFFIX)) {
                     dest.add(getClassFromFile(format, packageName, file, classLoader));
                 }
             }
-            return dest;
         }
-        return null;
+        return dest;
     }
 
     private Class<?> getClassFromFile(String format, String packageName, File file, ClassLoader classLoader) {
